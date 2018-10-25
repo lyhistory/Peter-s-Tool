4067764886fef8917a97129c780e9ac14003921e
lyhistory <lyhistory@gmail.com>
2018-10-25 23:07:16 +0800

    tmp

---
+++ desktop/src/test/java/io/stormbird/token/management/MagicLinkToolTest.java
@@ -0,0 +1,12 @@
+package io.stormbird.token.management;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MagicLinkToolTest {
+
+    @Test
+    public void parseMagicLink() {
+    }
+}
